07fbd6dc1480727c78353aa98b19482d95394853
coolCicada <1475395755@qq.com>
2023-10-27 02:07:51 +0800

    feat: 更新

--- components/layout.jsx
+++ components/layout.jsx
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Liang Sai';
+const name = '梁 赛';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
