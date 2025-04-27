103a3c126bb616566a6786eb70a1c2c87ce16157
ls-byte <15931171696@163.com>
2025-04-27 21:14:24 +0800

    feat: 添加卡片

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,18 @@
 export default function Blog() {
-    return <h1>Blog</h1>
+    return (
+        <div className="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+            <div>
+                <img className="size-48 shadow-xl rounded-md" src="https://tailwindcss.com/_next/static/media/cover.de1997f7.png" alt="123" />
+            </div>
+            <div className="flex flex-col items-center md:items-start">
+                <span className="text-2xl font-medium">Class Warfare</span>
+                <span className="font-medium text-sky-500">The Anti-Patterns</span>
+                <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+                    <span>No. 4</span>
+                    <span>·</span>
+                    <span>2025</span>
+                </span>
+            </div>
+        </div>
+    )
 }
